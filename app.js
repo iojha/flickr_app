7df84ea47a0b7f5dcee663c7b35dc27a047b05eb
Isha <isha.ojha@gmail.com>
2016-01-22 11:28:10 -0700

    api working

--- app.js
+++ app.js
@@ -4,55 +4,53 @@ angular.module('myApp', ['ngMessages'])
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
   })
 
-  .controller('searchCtrl', ['$scope', '$sce' ,'$timeout', '$q', '$http', function($scope, $sce, $timeout, $q, $http) {
+.controller('searchCtrl', ['$scope', '$sce', '$timeout', '$q', '$http', function($scope, $sce, $timeout, $q, $http) {
 
-      $scope.searchTerm = "";
+  $scope.trustSrc = function(src) {
+    return $sce.trustAsResourceUrl(src);
+  };
 
-      $scope.trustSrc = function(src) {
-	  return $sce.trustAsResourceUrl(src);
-	  };
+  //
 
-//
+  function wait() {
+    return $q(function(resolve, reject) {
+      $timeout(function() {
+        resolve();
+      }, 2000);
+    })
+  }
 
-    function wait() {
-        return $q(function(resolve, reject) {
-          $timeout(function() {
-            resolve();
-          }, 2000);
-        })
-      }
+  function notify() {
+    $scope.notifySearching = true;
+    return wait().then(function() {
+      $scope.notifySearching = false;
+    });
+  }
 
-     function notify() {
-        $scope.notifySearching = true;
-        return wait().then(function() {
-          $scope.notifySearching = false;
+
+  $scope.submit = function(query) {
+    $scope.query = query;
+    var url = "https://api.flickr.com/services/rest";
+    var params = {
+      method: 'flickr.photos.search',
+      api_key: "10911f9d429e68fc6375a103f28c10e5",
+      tags: query,
+      format: 'json',
+      nojsoncallback: 1
+    };
+
+    $http({
+        method: 'GET',
+        url: url,
+        params: params
+      })
+      .then(function(response) {
+          $scope.results = response.data.photos.photo;
+          console.log($scope.results);
+        },
+        function(response) {
+          alert('error');
         });
-      }
-
-
-    $scope.submit = function(query) {
-  	$scope.query = query;
-	  var url = "https://api.flickr.com/services/rest";
-	  var params = {
-	  	  method: 'flickr.photos.search',
-		  api_key: "10911f9d429e68fc6375a103f28c10e5",
-		  tags: query,
-		  format: 'json',
-		  nojsoncallback: 1
-	  };
-
-	  $http({
-		  method: 'GET',
-		  url: url,
-		  params: params
-	  })
-	  .then(function(response) {
-		  $scope.results = response.data.photos.photo;
-		  console.log($scope.results);
-	  },
-	  function(response) {
-		  alert('error');
-	  });
   };
 
 }]);
